6f7adc5de3f4200a1ecf022f9ecfbbcbab5eba2c
Say Cheong <greymistcube@gmail.com>
2019-04-21 07:37:35 +0900

    fixed faulty sensor

--- ai/neat/genome.py
+++ ai/neat/genome.py
@@ -11,18 +11,18 @@ class Genome:
         # fixed bias for simplicity
         # for optimal search, inputs should be normalized in advance
         self.x_bias = 1
-        self.h_bias = 1
+        # self.h_bias = 1
 
         if random_weights:
             self.w1 = np.random.random((
                 self.h_dim, self.x_dim + 1
                 )) * 2 - 1
             self.w2 = np.random.random((
-                self.y_dim, self.h_dim + 1
+                self.y_dim, self.h_dim
                 )) * 2 - 1
         else:
             self.w1 = np.zeros((self.h_dim, self.x_dim + 1))
-            self.w2 = np.zeros((self.y_dim, self.h_dim + 1))
+            self.w2 = np.zeros((self.y_dim, self.h_dim))
         return
 
     # x, h, and y are input vector, hidden vector, and output vector respectively
@@ -38,7 +38,7 @@ class Genome:
         # sigmoid activation commented out below
         # h = 1 / (1 + np.exp(-1 * h))
         h = h * (h > 0)
-        h = np.append(self.h_bias, h)
+        # h = np.append(self.h_bias, h)
 
         # multiply by weight and push to output
         y = np.dot(self.w2, h.reshape(-1, 1))
